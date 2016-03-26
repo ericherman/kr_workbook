2485a86db2e240a8b2ac06579cecd8695c3a8da2
Eric Herman <eric@freesa.org>
2016-03-26 19:03:35 +0100

    exercise 1.5 print f2c descending

---
+++ fahr-celsius-descending.c
@@ -0,0 +1,19 @@
+/* print Fahrenheit-Celsius table
+     for f = 0, 20, ..., 300 */
+main()
+{
+	int lower, upper, step;
+	float fahr, celsius;
+
+	lower = 0;	/* lower limit of temperature table */
+	upper = 300;	/* upper limit */
+	step = 20;	/* step size */
+
+	printf("fahr celsius\n");
+	fahr = upper;
+	while (fahr >= lower) {
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%4.0f %6.1f\n", fahr, celsius);
+		fahr = fahr - step;
+	}
+}
