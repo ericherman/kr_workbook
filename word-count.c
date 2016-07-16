5ce20b29e5a2a70529929ab041fd46c018a1a136
Eric Herman <eric@freesa.org>
2016-07-16 10:50:28 +0200

    fix typo in word-count.c

--- word-count.c
+++ word-count.c
@@ -17,7 +17,7 @@ main()
 			inword = YES;
 			++nw;
 		} else if ((inword == YES)
-			   && !((c >= 'A' && c <= 'Z') || (c >= 'z' && c <= 'z')
+			   && !((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
 				|| (c >= '0' && c <= '9') || c == '\'')) {
 			inword = NO;
 		}
